036f248d9bc7e28fc949344b9339e6dfc114f651
MickeyTC <MickeyTC@outlook.com>
2018-07-20 17:45:44 +0700

    Move state from TodoList to App

--- src/App.js
+++ src/App.js
@@ -5,6 +5,49 @@ import "./App.css";
 import TodoList from "./components/TodoList";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoList: [
+        {
+          id: "asd",
+          title: "asdas",
+          description: "asdww",
+          date: new Date(2018, 11, 1),
+          completed: false
+        },
+        {
+          id: "asd2",
+          title: "asdas2",
+          description: "asdww2",
+          date: null,
+          completed: true
+        }
+      ]
+    };
+    this.toggleTodoCompleted = this.toggleTodoCompleted.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+  }
+
+  toggleTodoCompleted(id) {
+    this.setState(prevState => {
+      const newTodoList = prevState.todoList.map(todo => ({ ...todo }));
+      const newTodo = newTodoList.find(todo => todo.id === id);
+      if (newTodo === undefined) return;
+      newTodo.completed = !newTodo.completed;
+      return { todoList: newTodoList };
+    });
+  }
+
+  deleteTodo(id) {
+    this.setState(prevState => {
+      const newTodoList = prevState.todoList
+        .map(todo => ({ ...todo }))
+        .filter(todo => todo.id !== id);
+      return { todoList: newTodoList };
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,7 +55,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <TodoList />
+        <TodoList
+          todoList={this.state.todoList}
+          toggleTodoCompleted={this.toggleTodoCompleted}
+          deleteTodo={this.deleteTodo}
+        />
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,68 +1,26 @@
-import React, { Component } from "react";
-import { List } from "@material-ui/core";
+import React from "react";
+import { List, Input } from "@material-ui/core";
 
 import TodoListItem from "./TodoListItem";
 
-class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todoList: [
-        {
-          id: "asd",
-          title: "asdas",
-          description: "asdww",
-          date: new Date(2018, 11, 1),
-          completed: false
-        },
-        {
-          id: "asd2",
-          title: "asdas2",
-          description: "asdww2",
-          date: null,
-          completed: true
-        }
-      ]
-    };
-    this.toggleTodoCompleted = this.toggleTodoCompleted.bind(this);
-    this.deleteTodo = this.deleteTodo.bind(this);
-  }
-
-  toggleTodoCompleted(id) {
-    this.setState(prevState => {
-      const newTodoList = prevState.todoList.map(todo => ({ ...todo }));
-      const newTodo = newTodoList.find(todo => todo.id === id);
-      if (newTodo === undefined) return;
-      newTodo.completed = !newTodo.completed;
-      return { todoList: newTodoList };
-    });
-  }
-
-  deleteTodo(id) {
-    this.setState(prevState => {
-      const newTodoList = prevState.todoList
-        .map(todo => ({ ...todo }))
-        .filter(todo => todo.id !== id);
-      return { todoList: newTodoList };
-    });
-  }
-
-  render() {
-    return (
+const TodoList = ({ todoList, toggleTodoCompleted, deleteTodo }) => {
+  return (
+    <div>
       <List>
-        {this.state.todoList.map(todo => {
+        {todoList.map(todo => {
           return (
             <TodoListItem
               key={todo.id}
               todo={todo}
-              onToggleCompleted={this.toggleTodoCompleted}
-              onDeleteTodo={this.deleteTodo}
+              onToggleCompleted={toggleTodoCompleted}
+              onDeleteTodo={deleteTodo}
             />
           );
         })}
+        <Input />
       </List>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default TodoList;
