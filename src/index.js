277dd259201103eb8b1f46f152d0647c87940f1d
MickeyTC <MickeyTC@outlook.com>
2018-07-24 02:58:33 +0700

    Use redux-persist to save local

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import configureStore from "./store/configureStore";
+import { PersistGate } from "redux-persist/integration/react";
 
-const store = configureStore();
+const { store, persistor } = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,19 @@
 import reducer from "../reducers/index";
 import { createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export default function configureStore() {
-  return createStore(
-    reducer,
+const persistConfig = {
+  key: "root",
+  storage
+};
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export default () => {
+  const store = createStore(
+    persistedReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
-}
+  const persistor = persistStore(store);
+  return { store, persistor };
+};
