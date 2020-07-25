c4a74267b476096bd8c6cb81252a0ce91362dcf0
Teerapat Chintanakit <t.chintanakit@gmail.com>
2020-07-26 05:19:02 +0700

    Updates

---
+++ precache-manifest.7cdc5700654f0f314909b4340f0e911d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f14a181e31f0b639e50ca08cf3add1",
+    "url": "/reminders-react/index.html"
+  },
+  {
+    "revision": "82236e5b397d0bfd1879",
+    "url": "/reminders-react/static/css/main.1264184f.chunk.css"
+  },
+  {
+    "revision": "e0269a145c47f84e2a57",
+    "url": "/reminders-react/static/js/2.53e29c58.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/reminders-react/static/js/2.53e29c58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82236e5b397d0bfd1879",
+    "url": "/reminders-react/static/js/main.ce9a6b64.chunk.js"
+  },
+  {
+    "revision": "220810739ee6ff01abe3",
+    "url": "/reminders-react/static/js/runtime-main.8b3c3e19.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/reminders-react/static/media/logo.ee7cd8ed.svg"
+  }
+]);
